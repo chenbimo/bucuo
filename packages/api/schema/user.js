813f598dd4ce0ec8ad5e053047df70278ec950c9
陈随易 <chenbimo@gmail.com>
2025-06-14 12:04:00 +0800

    1

--- packages/api/schema/user.js
+++ packages/api/schema/user.js
@@ -9,14 +9,33 @@ import { createValidator } from 'bunfly';
  */
 export const user = {
     // 用户登录
-    login: () => createValidator().string('username', { min: 1 }).string('password', { min: 6 }).build(),
+    login: () =>
+        createValidator() //
+            .string('username', { min: 1 })
+            .string('password', { min: 6 })
+            .build(),
 
     // 用户创建
-    create: () => createValidator().string('username', { min: 1 }).string('password', { min: 6 }).string('email', { email: true, optional: true }).string('nickname', { optional: true }).build(),
+    create: () =>
+        createValidator() //
+            .string('username', { min: 1 })
+            .string('password', { min: 6 })
+            .string('email', { email: true, optional: true })
+            .string('nickname', { optional: true })
+            .build(),
 
     // 用户更新
-    update: () => createValidator().number('id', { int: true, positive: true }).string('username', { min: 1, optional: true }).string('email', { email: true, optional: true }).string('nickname', { optional: true }).build(),
+    update: () =>
+        createValidator() //
+            .number('id', { int: true, positive: true })
+            .string('username', { min: 1, optional: true })
+            .string('email', { email: true, optional: true })
+            .string('nickname', { optional: true })
+            .build(),
 
     // 用户资料
-    profile: () => createValidator().string('token', { optional: true }).build()
+    profile: () =>
+        createValidator() //
+            .string('token', { optional: true })
+            .build()
 };
