3dc7cc4f0ddc2b3ac632ce6c756063f2e8920e19
陈随易 <chenbimo@gmail.com>
2025-06-19 22:55:10 +0800

    增肌日志插件

---
+++ plugins/logger.js
@@ -0,0 +1,15 @@
+import { Env } from '../config/env.js';
+import { colors } from '../utils/colors.js';
+import { logger } from '../utils/logger.js';
+
+export default {
+    order: 1,
+    async onInit(bucuo, req) {
+        try {
+            return logger;
+        } catch (error) {
+            console.error(`${colors.error} 数据库连接失败:`, error.message);
+            process.exit();
+        }
+    }
+};
